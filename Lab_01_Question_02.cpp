abc186cc33d6fb7ba2c300e2def8cfad31d3e9c5
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-08-24 09:37:16 +0530

    Create Lab_01_Question_02.cpp
    Write a C++ program to enter two numbers and find their sum.

---
+++ Lab_01_Question_02.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program to enter two numbers and find their sum.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b;
+	cout << "Enter two numbers: " << endl;
+	cin >> a >> b;
+	cout << "\nSum of " << a << " and " << b << ": " << (a+b);
+	return 0;
+}
