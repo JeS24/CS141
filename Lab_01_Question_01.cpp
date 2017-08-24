ee720d6e03731b38b945fd4e599f64d9a7d7aab7
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-08-24 09:31:29 +0530

    Create Lab_01_Question_01.cpp
    1. Write a C++ program to perform input/output of all basic data types.

---
+++ Lab_01_Question_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Int;
+	char Char;
+	float Float;
+	double Double;
+	bool Bool;
+	cout << "INPUT-OUTPUT OF BASIC DATA-TYPES" << endl;
+	cout << "Enter in the following order: int -> char -> float -> double -> bool \n";
+	cin >> Int >> Char >> Float >> Double >> Bool;
+	cout << "\nHere's what you entered: \n";
+	cout  << "Integer: " << Int << "\nChar: " << Char << "\nFloat: " << Float << "\nDouble: " << Double << "\nBoolean: " << Bool;
+	return 0;
+}
