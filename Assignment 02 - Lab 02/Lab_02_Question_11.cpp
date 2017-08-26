7a7cf837f5bb8c7d7c18b40486705454cab42d64
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-08-26 14:06:31 +0530

    Create Lab_02_Question_11.cpp
    11. Write a C++ program to enter P, T, R and calculate Simple Interest.

---
+++ Assignment 02 - Lab 02/Lab_02_Question_11.cpp
@@ -0,0 +1,17 @@
+//A C++ program to enter Pricipal Amount, Time & Rate, and then calculate Simple Interest
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double prinA, rate, timeP, SI;
+	cout << "Enter the Principal Amount: " << endl;
+	cin >> prinA;
+	cout << "Enter the Rate of Interest: " << endl;
+	cin >> rate;
+	cout << "Enter the Time Period, in years: " << endl;
+	cin >> timeP;
+	SI = (prinA*rate*timeP)/100;
+	cout << "\nSimple Interest on a principal amount of ₹" << prinA << " for a time period of " << timeP << " years, at a rate of interest of " << rate << "%: ₹" << SI << endl;
+	cout << "And, total Amount, to be repayed: ₹" << (prinA+SI);
+}
