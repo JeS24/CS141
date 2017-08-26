f656814d6465ad09457267c6d460ef60a752620e
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-08-26 11:35:35 +0530

    Update Lab_02_Question_01.cpp

--- Assignment 02 - Lab 02/Lab_02_Question_01.cpp
+++ Assignment 02 - Lab 02/Lab_02_Question_01.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	double len;
-	cout << "Enter length in centimetres: " << endl;
+	cout << "Enter length in centimetres:" << endl;
 	cin >> len;
 	cout << "\nLength in metres: " << (len/100) << " m" << endl << "Length in kilometres: " << (len/100000) << " km";
 	return 0;
