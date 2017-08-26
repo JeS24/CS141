32a9ad5588f09e6913aa1b53ca425abe4dd9cac7
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-08-26 11:24:37 +0530

    Create Lab_02_Question_04.cpp
    4. Write a C++ program to convert days into years, weeks and days.

---
+++ Assignment 02 - Lab 02/Lab_02_Question_04.cpp
@@ -0,0 +1,33 @@
+//A C++ program to convert days into years, weeks and days
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double n_days;
+int n_years, n_weeks;
+int main() {
+	cout << "Enter number of days:" << endl;
+	cin >> n_days;
+	cout << endl << n_days << " refers to " ;
+	return 0;
+}
+
+int getYears()
+{
+	int n_4_years = (n_days/365)/4;
+	if (n_4_years < 1) //Because >4 years mean >1460 days - accounting for the leap year
+		return n_4_years;
+	else
+		return n_4_years;
+}
+
+int getWeeks()
+{
+	return (n_years*52);
+}
+
+int getDays()
+{
+	if ()
+}
