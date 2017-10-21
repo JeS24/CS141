0246b68579119900b600df8454c7af2d6ad8c584
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-10-21 15:42:28 +0530

    Minor edits

--- Assignment 03 - Lab 03/Lab_03_Question_39-t.cpp
+++ Assignment 03 - Lab 03/Lab_03_Question_39-t.cpp
@@ -2,12 +2,25 @@
 #include <cstdlib>
 using namespace std;
 
+void getFreq(string st)
+{
+    long arr[10]; //Stores frequencies of digits
+    for (int i = 0 ; i < 10 ; i++)
+        arr[i] = 0;
+    for (int i = 0 ; i < st.length() ; i++)
+        for (int j = 0 ; j < st.length() ; j++)
+            if (st[i] == st[j])
+                arr[st[i]-48]++;
+    for (int i = 0 ; i < 10 ; i++)
+        if (arr[i] != 0)
+            cout << "Frequency of " << st[i] << ": " << arr[i] << endl;
+}
+
 int main()
 {
     string num;
     cout << "Enter a number: ";
     cin >> num;
-    long arr[num.size()];
-    for (int i = num ; i > 0 ; i /= 10)
-        arr[i] = atoi(num[i]);
+    cout << endl;
+    getFreq(num);
 }
