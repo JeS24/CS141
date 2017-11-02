2cf9e0271ac92954114394a8beb31e39c55e7950
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-11-02 11:31:55 +0530

    Update

---
+++ Assignment 08 - Lab 07/Lab_07_Question_01-2.cpp
@@ -0,0 +1,10 @@
+class Box {
+    public:
+        double length; //Length of a box
+        double breadth; //Breadth of a box
+        double height; //Height of a box
+
+    double getVolume(void) {
+        return length*breadth*height;
+    }
+};
