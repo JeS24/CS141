3d625a7a596ae3c6282309b9b5c3eea57b49fdd4
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-11-20 18:52:12 +0530

    Update Lab_08_Question_01.cpp

--- Assignment 09 - Lab 08/Lab_08_Question_01.cpp
+++ Assignment 09 - Lab 08/Lab_08_Question_01.cpp
@@ -1,17 +1,95 @@
+#include <iostream>
+using namespace std;
+
 class Node {
-    private:
-        int data;
-
     public:
-        Node();
-        void test();
+        int data;
+        Node *next;
 };
-void Node::test() {
-    return;
-}
 
+class LinkedList {
+    private:
+        Node *head, *tail;
+    
+    public:
+        LinkedList() {
+            head = NULL;
+            tail = NULL;
+        }
+        
+        void addNode(int val) {
+            Node *temp = new Node;
+            temp->data = val;
+            temp->next = NULL;
+            if (head == NULL) {
+                head = temp;
+                tail = temp;
+                temp = NULL;
+            }
+            else {
+                tail->next = temp;
+                tail = temp;
+            }
+        }
+        
+        void display() {
+            Node *temp = new Node;
+            temp = head;
+            while (temp != NULL) {
+                cout << temp->data << "\t";
+                temp = temp->next;
+            }
+        }
+        
+        void insertNode(int pos, int val) {
+            Node *pre, *cur, *temp;
+            cur = head;
+            if (pos == 1) {
+                temp->data = val;
+                temp->next = head;
+                head = temp;
+            }
+            else {
+                for (int i = 1 ; i < pos ; i++) {
+                    pre = cur;
+                    cur = cur->next;
+                }
+                temp->data = val;
+                pre->next = temp;
+                temp->next = cur;
+            }
+        }
+
+        void deleteNode(int pos)
+        {
+            Node *pre, *cur;
+            cur = head;
+            if (pos == 1) {
+                head = head->next;
+                delete cur;
+            }
+            else {
+                for (int i = 1 ; i < pos ; i++){
+                    pre = cur;
+                    cur = cur->next;
+                }
+                pre->next = cur->next;
+                if (cur->next == NULL)
+                    tail = pre;
+            }
+        }
+};
 
 int main()
 {
-    return 0;
+    int b = 0, size = 0;
+    LinkedList M;
+    cout << "Enter the size of the Linked List: ";
+    cin >> size;
+    cout << "\nEnter the data of each node in the list: " << endl;
+    for (int j = 0; j < size ; j++) {
+        cin >> b;
+        M.addNode(b);
+    }
+    M.display();
 }
