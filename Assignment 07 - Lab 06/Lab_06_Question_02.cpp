8436efb1c5bdc58fb516ff2b1f04fefd45683662
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-11-20 20:12:18 +0530

    Minor edit

--- Assignment 07 - Lab 06/Lab_06_Question_02.cpp
+++ Assignment 07 - Lab 06/Lab_06_Question_02.cpp
@@ -9,10 +9,10 @@ int main()
     cout << "\nNow, enter the value of 'b': ";
     cin >> b;
     p = &a;
-    cout << "\nValues of 'a', 'b' and '*p', respectively, are: "  a << ", " << b << " " << *p << endl;
+    cout << "\nValues of 'a', 'b' and '*p', respectively, are: "  << a << ", " << b << " and " << *p << endl;
     a = 2;
     b = 3;
-    cout << "After setting 'a' to 2, and 'b' to 3, values of 'a', 'b' and '*p', respectively, are: "  a << ", " << b << " " << *p << endl;
-    b = *p;
-    cout << "After poiting 'p' to 'b', values of 'a', 'b' and '*p', respectively, are: "  a << ", " << b << " " << *p << endl;
+    cout << "After setting 'a' to 2, and 'b' to 3, values of 'a', 'b' and '*p', respectively, are: " << a << ", " << b << " and " << *p << endl;
+    p = &b;
+    cout << "After pointing 'p' to 'b', values of 'a', 'b' and '*p', respectively, are: " << a << ", " << b << " and " << *p << endl;
 }
