685b22664fdf94edd547af71e0115984719bdb4d
JeS24 <31299799+JeS24@users.noreply.github.com>
2017-11-20 21:20:40 +0530

    Fix required for Q. 6

--- Assignment 07 - Lab 06/Lab_06_Question_05.cpp
+++ Assignment 07 - Lab 06/Lab_06_Question_05.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     char *str, *ptr;
-    cout << "Input a string: " << endl; //" *** stack smashing detected *** " -> If Length >>>> (Much greater than) 20
+    cout << "Input a string: "; //" *** stack smashing detected *** " -> If Length >> (Much greater than) 20
     cin >> str;                         // ^^^^ When "char str[20]" is used | No such issue with "char *str"
     ptr = str;
     cout << "\nRequired design: " << endl;
-    for (int j = 0 ; *(ptr+j) != '\0' ; j++) {
-        for (int i = j ; *(ptr+i) != '\0' ; i++)
-            cout << *(ptr+i);
+    for (int i = 0 ; *(ptr+i) != '\0' ; i++) {
+        for (int j = i ; *(ptr+j) != '\0' ; j++)
+            cout << *(ptr+j);
         cout << endl;
     }
 }
--- Assignment 07 - Lab 06/Lab_06_Question_06.cpp
+++ Assignment 07 - Lab 06/Lab_06_Question_06.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main()
 {
-    char *str, *ptr;
-    cout << "Input a string: " << endl; //" *** stack smashing detected *** " -> If Length >>>> (Much greater than) 20
-    cin >> str;                         // ^^^^ When "char str[20]" is used | No such issue with "char *str"
-    ptr = str;
-    for (int j = 20 ; j > 0 ; j--) {
-        if (*(ptr+j) == '\0')
-            j--;
-        for (int i = j ; i <= 20 ; i++)
-            cout << *(ptr+i);
+    char  str[20], *p;
+    cout << "Input a string, comtaining 20 characters: "; //" *** stack smashing detected *** " -> If Length >> (Much greater than) 20
+    cin >> str;                                      // ^^^^ When "char str[20]" is used | No such issue with "char *str"
+    p = str;
+    //Beautify
+    for (int i = 20 ; i >= 0 ; i--) { //Code doesn't work for length < 20
+        while (*(p+i) == '\0')
+            i--;
+        for (int j = i ; j <= 20 ; j++)
+            cout << *(p+j);
         cout << endl;
     }
 }
